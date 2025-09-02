53c4f00dd040ffab535c595f83f7b8668a7e385d
Duy-cpu <mynhduy2692013@gmail.com>
2025-09-02 09:14:18 +0700

    Create webserver.py

---
+++ webserver.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "✅ Bot is running!"
+
+def keep_alive():
+    port = int(os.environ.get("PORT", 8080))  # Render cung cấp PORT qua biến môi trường
+    app.run(host="0.0.0.0", port=port)
